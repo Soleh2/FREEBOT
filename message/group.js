1d673fe979c2ea2f4a0ce1f5572a663311756a88
Soleh2 <rifando.p.p0@gmail.com>
2021-11-01 11:13:16 +0700

    FREEBOT

---
+++ message/group.js
@@ -0,0 +1,80 @@
+const {
+	MessageType
+} = require("@adiwajshing/baileys");
+const fs = require("fs-extra")
+
+const { getBuffer } = require('../lib/myfunc')
+const { color, bgcolor } = require('../lib/color')
+
+let setting = JSON.parse(fs.readFileSync('./setting.json'))
+
+module.exports = welcome = async (Marcel, anu) => {
+	    const welkom = JSON.parse(fs.readFileSync('./database/group/welcome.json'))
+	    const isWelcome = welkom.includes(anu.jid)	    
+	    if (!isWelcome) return
+		try {
+			const mdata = await Marcel.groupMetadata(anu.jid)
+			console.log(anu)
+			if (anu.action == 'add') {
+				num = anu.participants[0]
+				try {
+					ppimg = await Marcel.getProfilePicture(`${anu.participants[0].split('@')[0]}@c.us`)
+				} catch {
+					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+				} 
+				let buff = await getBuffer(ppimg)
+				buttons = [
+                { buttonId: `98`, buttonText: { displayText: "Free Bot" }, type: 1 },]
+                imageMsg = (
+                await Marcel.prepareMessageMedia(buff, "imageMessage", {
+                thumbnail: buff,
+               })
+            ).imageMessage;
+          buttonsMessage = {
+          contentText: `━━━ *「 WELCOME 」* ━━━
+				
+\`\`\`𝘽𝙚𝙗𝙖𝙣 𝘽𝙖𝙧𝙪\`\`\` \n \`\`\`Nama :\`\`\` \n \`\`\`Askot : \`\`\` \n \`\`\`Umur :\`\`\` \n \`\`\`Status :\`\`\` \n\n - [ Free Bot ] -`,
+          footerText: "Enjoy Lurrd",
+          imageMessage: imageMsg,
+          buttons: buttons,
+          headerType: 4,
+        }
+        prep = await Marcel.prepareMessageFromContent(
+          mdata.id,
+          { buttonsMessage },
+          {}
+        )
+        Marcel.relayWAMessage(prep)     				             
+				} else if (anu.action == 'remove') {
+				num = anu.participants[0]
+				try {
+					ppimg = await Marcel.getProfilePicture(`${num.split('@')[0]}@c.us`)
+				} catch {
+					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
+				}
+				let buff = await getBuffer(ppimg)
+				buttons = [
+                { buttonId: `98`, buttonText: { displayText: "Free Bot" }, type: 1 },]
+                imageMsg = (
+                await Marcel.prepareMessageMedia(buff, "imageMessage", {
+                thumbnail: buff,
+               })
+            ).imageMessage;
+          buttonsMessage = {
+          contentText: `Sayonara @${num.split('@')[0]}👋`,
+          footerText: "Beban keluar",
+          imageMessage: imageMsg,
+          buttons: buttons,
+          headerType: 4,
+        }
+        prep = await Marcel.prepareMessageFromContent(
+          mdata.id,
+          { buttonsMessage },
+          {}
+        )
+        Marcel.relayWAMessage(prep)     		
+			}
+		} catch (e) {
+			console.log('Error : %s', color(e, 'red'))
+		}
+	}
---
+++ message/help.js
@@ -0,0 +1,240 @@
+exports.gameMenu = (prefix) => {
+    return `
+ ━━━━ *GAME-MENU* ━━━━
+ 
+🐣 ${prefix}slot
+🐣 ${prefix}limitgame
+🐣 ${prefix}gelud @tag
+🐣 ${prefix}tictactoe @tag
+🐣 ${prefix}siapaaku
+🐣 ${prefix}family100
+🐣 ${prefix}kuismath
+🐣 ${prefix}asahotak
+🐣 ${prefix}tebaklirik
+🐣️ ${prefix}tebaklagu
+🐣 ${prefix}tebakkata
+🐣${prefix}susunkata
+🐣 ${prefix}kimiakuis
+🐣 ${prefix}caklontong
+🐣 ${prefix}tebakjenaka
+🐣 ${prefix}tebakanime
+🐣 ${prefix}tebaktebakan
+🐣️ ${prefix}tebakgambar
+🐣 ${prefix}tebakbendera
+🐣️ ${prefix}suit *batu/kertas/gunting*
+`
+}
+exports.funMenu = (prefix) => {
+    return `
+━━━━ *FUN-MENU* ━━━━
+
+🐣️ ${prefix}mining
+🐣 ${prefix}togel
+🐣 ${prefix}cekwatak
+🐣️ ${prefix}cekmati [nama]
+🐣 ${prefix}wangy [nama]
+🐣 ${prefix}citacita
+🐣 ${prefix}toxic
+🐣️ ${prefix}truth
+🐣 ${prefix}dare
+🐣️ ${prefix}apakah
+🐣️ ${prefix}bisakah
+🐣️ ${prefix}kapankah
+🐣️ ${prefix}rate
+🐣 ${prefix}jadian
+🐣️ ${prefix}cantik
+🐣 ${prefix}ganteng
+🐣️ ${prefix}beban
+🐣️ ${prefix}babi
+🐣️ ${prefix}cekganteng
+🐣 ${prefix}cekcantik
+
+`
+}
+exports.otherMenu = (prefix) => {
+    return `
+━━━━ *OTHER-MENU* ━━━━
+
+🐣️ ${prefix}brainly *query*
+🐣 ${prefix}shopee *product*
+🐣️ ${prefix}playstore *query*
+🐣 ${prefix}ssweb *query*
+🐣️ ${prefix}google *query*
+🐣️ ${prefix}image *query*
+🐣 ${prefix}pinterest *query*
+🐣️ ${prefix}nulis *teks*
+🐣 ${prefix}iguser *ussername*
+🐣️ ${prefix}igstalk *username*
+🐣️ ${prefix}githubstalk *username*
+🐣️ ${prefix}tiktokstalk *ussername*
+🐣️ ${prefix}img2url *reply foto*
+🐣 ${prefix}ytsearch *query*
+
+`
+}
+exports.stickerMenu = (prefix) => {
+    return `
+━━━━ *STICKER-MENU* ━━━━
+
+🐣️ ${prefix}dadu
+🐣 ${prefix}doge
+🐣️ ${prefix}toimg
+🐣️ ${prefix}patrick
+🐣️ ${prefix}garwgura
+🐣️ ${prefix}ttg *teks*
+🐣 ${prefix}attp *teks*
+🐣 ${prefix}stickeranime
+🐣️ ${prefix}semoji *emoji*
+🐣 ${prefix}sticker *reply foto/video*
+🐣️ ${prefix}smeme *teks|teks*
+🐣 ${prefix}swm *pack|author*
+🐣 ${prefix}take *pack|author* 
+🐣 ${prefix}tovideo *reply sgif*
+
+`
+}
+exports.wibuMenu = (prefix) => {
+    return `
+━━━━ *WIBU-MENU* ━━━━
+
+🐣️ ${prefix}loli
+🐣️ ${prefix}manga
+🐣 ${prefix}anime 
+🐣️ ${prefix}lolivideo
+🐣️ ${prefix}husbu
+🐣️ ${prefix}waifu
+🐣️ ${prefix}milf
+🐣️ ${prefix}neko
+🐣️ ${prefix}kanna
+🐣 ${prefix}sagiri
+️🐣 ${prefix}hentai
+🐣 ${prefix}cosplay
+🐣️ ${prefix}wallnime
+🐣️ ${prefix}kusonime
+🐣️ ${prefix}megumin
+🐣️ ${prefix}otakudesu
+🐣️ ${prefix}doujindesu
+🐣️ ${prefix}storyanime
+🐣 ${prefix}nakanomiku
+🐣️ ${prefix}nakanonino
+🐣️ ${prefix}nakanoitsuki
+🐣️ ${prefix}otakuongoing
+🐣 ${prefix}nhentai *code*
+🐣️ ${prefix}nekopoi *link*
+🐣 ${prefix}nekopoi3d
+🐣️ ${prefix}nekopoicosplay
+🐣 ${prefix}nekopoisearch
+
+`
+}
+exports.ownerMenu = (prefix) => {
+    return `
+━━━━ *OWNER-MENU* ━━━━
+
+🐣️ ${prefix}bc *teks*
+🐣 ${prefix}term
+🐣 ${prefix}eval
+🐣 ${prefix}reset
+🐣 ${prefix}clearall
+🐣️ ${prefix}leaveall
+🐣 ${prefix}join *link gc*
+🐣️ ${prefix}shutdown
+🐣️ ${prefix}getquoted
+🐣️ ${prefix}addupdate *fiturnya*
+🐣 ${prefix}exif *nama|author*
+🐣️ ${prefix}sewa add/del *waktunya*
+🐣️ ${prefix}premium add @tag|nomor
+🐣️ ${prefix}premium del @tag|nomor
+
+`
+}
+
+exports.groupMenu = (prefix) => {
+    return`
+━━━━ *GROUP-MENU* ━━━━
+
+🐣️ ${prefix}groupsetting
+🐣️ ${prefix}afk *alasan*
+🐣️ ${prefix}ceksewa
+🐣️ ${prefix}kickall
+🐣️ ${prefix}infogrup
+🐣 ${prefix}promote
+🐣 ${prefix}demote
+🐣️ ${prefix}listonline
+🐣️ ${prefix}tagall *teks*
+🐣️ ${prefix}leave
+🐣 ${prefix}kick *reply*
+🐣 ${prefix}add *+62xxxxxx*
+🐣️ ${prefix}setgrupname
+🐣 ${prefix}setppgrup
+🐣️ ${prefix}setdesc
+🐣 ${prefix}sider *reply chat bot*
+🐣 ${prefix}hidetag *teks/reply teks*
+,
+`
+}
+exports.downloadMenu = (prefix) => {
+    return `
+━━━━ *DOWNLOAD-MENU* ━━━━
+
+🐣️ ${prefix}fb 
+🐣️ ${prefix}igdl 
+🐣️ ${prefix}igdl2 
+🐣️ ${prefix}twitter 
+🐣 ${prefix}tiktok 
+🐣 ${prefix}play 
+🐣️ ${prefix}ythd 
+🐣️ ${prefix}ytmp3 
+🐣 ${prefix}ytmp4 
+🐣 ${prefix}soundcloud 
+🐣 ${prefix}tiktoknowm 
+🐣️ ${prefix}tiktokaudio
+🐣️ ${prefix}mediafire 
+🐣️ ${prefix}nhentaipdf *code*
+
+`
+}
+exports.rulesBot = (prefix) => {
+    return `
+
+━━━━ *RULES BOT* ━━━━
+
+1. Jangan spam bot. 
+Sanksi: *WARN/SOFT BLOCK*
+
+2. Jangan telepon bot.
+Sanksi: *SOFT BLOCK*
+
+3. Jangan mengejek bot.
+Sanksi: *PERMANENT BLOCK*
+
+Jika sudah dipahami rules-nya, silakan ketik *#menu* untuk memulai!
+
+OWNER:
+wa.me/6281252848955
+`
+}
+exports.infoMenu = (prefix) => {
+return `
+━━━━ *INFO-MENU* ━━━━
+
+🐣 ${prefix}update
+🐣 ${prefix}level
+🐣 ${prefix}rules
+🐣 ${prefix}profile
+🐣 ${prefix}waktu
+🐣 ${prefix}botstat
+🐣️ ${prefix}sewabot
+🐣 ${prefix}listsewa
+🐣 ${prefix}owner
+🐣 ${prefix}ping
+🐣️ ${prefix}runtime
+️🐣 ${prefix}donasi
+🐣️ ${prefix}leaderboard
+🐣 ${prefix}cekpremium
+🐣️ ${prefix}listpremium
+🐣 ${prefix}sourcecode
+🐣 ${prefix}bugreport *keluhan*
+
+`
+} 
